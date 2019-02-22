c8daa79547bd3ea516fc3c1d8a747f446a927a53
Brunojob6 <brunojob6@gmail.com>
2019-02-22 11:31:56 -0300

    Add: configurando o dashboard com o state do redux

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,15 +8,17 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
+        //Extrair os atributos do summary ou destruct (atlho vs:dob)
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title="Dashboard" small="Versão 1.0"/>
                 <Content>
                     <Row>
                         <ValueBox cols="12 4" color="green" icon="bank"
-                            value="R$ 10" text="Total de Créditos"/>
+                            value={`R$ ${credit}`} text="Total de Créditos"/>
                         <ValueBox cols="12 4" color="red" icon="credit-cart"
-                            value="R$ 10" text="Total de Debitos"/>
+                            value={`R$ ${debt}`} text="Total de Debitos"/>
                         
                         <ValueBox cols="12 4" color="blue" icon="money"
                             value="R$ 10" text="Valor Consolidado"/>
@@ -25,5 +28,7 @@ class Dashboard extends Component {
         )
     }
 }
+//So pra melhorar a compreensão do fluxo do redux: 1- mapStateToProps 2- Renderiza a classe
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
 
-export default Dashboard
+export default connect(mapStateToProps)(Dashboard)
