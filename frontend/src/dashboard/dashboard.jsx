dc2c369766b90f8226143c2137d21df2144c7618
Brunojob6 <brunojob6@gmail.com>
2019-02-22 14:26:50 -0300

    Add: Criando a primeira funçao com redux e configurando a primeira middleware para as actions

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from "redux";
+
+import { getSummary } from './dashboardAction'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,6 +10,11 @@ import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+
+    componentWillMount() {
+        this.props.getSummary()
+    }
+
     render() {
         //Extrair os atributos do summary ou destruct (atlho vs:dob)
         const { credit, debt } = this.props.summary
@@ -21,14 +29,15 @@ class Dashboard extends Component {
                             value={`R$ ${debt}`} text="Total de Debitos"/>
                         
                         <ValueBox cols="12 4" color="blue" icon="money"
-                            value="R$ 10" text="Valor Consolidado"/>
+                            value={`R$ ${credit - debt}`} text="Valor Consolidado"/>
                     </Row>
                 </Content>
             </div>
         )
     }
 }
-//So pra melhorar a compreensão do fluxo do redux: 1- mapStateToProps 2- Renderiza a classe
-const mapStateToProps = state => ({ summary: state.dashboard.summary })
 
-export default connect(mapStateToProps)(Dashboard)
+//So pra melhorar a compreensão do fluxo do redux: 1- mapStateToProps, Actions  2- Renderiza a classe
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
